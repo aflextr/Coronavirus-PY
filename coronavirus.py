269a53db5882d6c983eb25007fd4dd7e0d6dd5c1
aflextr <eyup.elitass@gmail.com>
2021-08-11 20:02:46 +0000

    updated

--- coronavirus.py
+++ coronavirus.py
@@ -3,41 +3,35 @@ from bs4 import BeautifulSoup
 import os
 
 
+def country(value):
+    try:
+        r = requests.get("https://www.worldometers.info/"+value+"/")
+        Soup = BeautifulSoup(r.content ,"html.parser")
+    except:
+        print("Network Error.")
+    print("--------------"+value+"----------------")
+    for link in Soup.find_all(id="maincounter-wrap"):
+        print(link.text)
+    pass
+
+
+
+
+
 def main():
-    print("**************CORONAVİRUS WORLD CASE****************")
-    print("1-World")
-    print("2-italy")
-    print("3-USA")
-    print("4-Turkey")
-    print("5 Other Countries")
-    print("**************CORONAVİRUS WORLD CASE****************")
+    file = open("motd.txt", "r")
+    print(file.read())
     
     al = int(input("Select:"))
 
     if al == 1:
-        r = requests.get("https://www.worldometers.info/coronavirus/")
-        Soup = BeautifulSoup(r.content ,"html.parser")
-        for link in Soup.find_all(id="maincounter-wrap"):
-            print(link.text)
-            pass
+        country("coronavirus")
     elif al == 2:
-        r = requests.get("https://www.worldometers.info/coronavirus/country/italy/")
-        Soup = BeautifulSoup(r.content ,"html.parser")
-        for link in Soup.find_all(id="maincounter-wrap"):
-            print(link.text)
-            pass
+        country("coronavirus/country/italy")
     elif al == 3:
-        r = requests.get("https://www.worldometers.info/coronavirus/country/us/")
-        Soup = BeautifulSoup(r.content ,"html.parser")
-        for link in Soup.find_all(id="maincounter-wrap"):
-            print(link.text)
-            pass
+        country("coronavirus/country/us")
     elif al == 4:
-        r = requests.get("https://www.worldometers.info/coronavirus/country/turkey/")
-        Soup = BeautifulSoup(r.content ,"html.parser")
-        for link in Soup.find_all(id="maincounter-wrap"):
-            print(link.text)
-            pass
+        country("coronavirus/country/turkey")
     elif al == 5:
         r = requests.get("https://www.worldometers.info/coronavirus/")
         Soup = BeautifulSoup(r.content ,"html.parser")
@@ -48,13 +42,10 @@ def main():
             print(" ")          
             pass
     elif al == 6:
-        r = requests.get("https://www.worldometers.info/coronavirus/country/samoa/")
-        soup = BeautifulSoup(r.content,"html.parser")
-        find = soup.find("div" , attrs={"class":"maincounter-number"})
-        print("Coronavirus Cases", +find.text, end="")
+        namecountry = str(input("Country Name : "))
+        country("coronavirus/country/"+namecountry)
         pass
-        
-        
+              
         
 main()
 
